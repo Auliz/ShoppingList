bbeee8ce9f06f0231e3541de5d40aaefdc7752f1
Auliz <joseph.auz77@gmail.com>
2020-10-05 02:36:55 -0500

    first commit

---
+++ client/src/store.js
@@ -0,0 +1,16 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './reducers/index';
+
+const initialState = {};
+
+const middlewares = [thunk];
+const middlewareEnhancer = applyMiddleware(...middlewares);
+
+const enhancers = [middlewareEnhancer];
+const composedEnhancers = composeWithDevTools(...enhancers);
+
+const store = createStore(rootReducer, initialState, composedEnhancers);
+
+export default store;
---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    'mongodb+srv://joe:joe123@mernlearn.8yomh.mongodb.net/mernLearn?retryWrites=true&w=majority',
+};
